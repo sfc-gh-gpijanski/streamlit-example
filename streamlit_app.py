5d37f77bf7c2f8d5110b4f69d6968fc41475b98a
Grzegorz Pijanski <grzegorz.pijanski@snowflake.com>
2024-01-12 10:53:28 +0100

    Add st.exception

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,9 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
+e = RuntimeError('This is an exception for testing user_error flag')
+st.exception(e)
+
 
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)

133058ccb649d1edf1d13862ebcbbdca5d11e9d3
Grzegorz Pijanski <grzegorz.pijanski@snowflake.com>
2024-02-14 09:24:20 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,9 +15,6 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
-e = RuntimeError('This is a cool exception of type RuntimeError')
-st.exception(e)
-
 with st.echo(code_location='below'):
     total_points = st.slider("Numbers of points in spirals", 1, 5000, 2000)
     num_turns = st.slider("Numbers of turns in spirals", 1, 100, 9)
